f9aaccefa49c8a8082993bf935e5b33871f6a64e
dewslyse <dewslyse@gmail.com>
2022-06-10 17:21:12 +0000

    Add all method

--- enumerable.rb
+++ enumerable.rb
@@ -6,18 +6,3 @@ module MyEnumerable
     true
   end
 end
-
-class MyList
-  include MyEnumerable
-  def initialize(list) 
-    @list = list
-  end
-
-  def each
-    @list.each do |item|
-      yield item
-    end
-  end
-end
-
-list = MyList.new(1, 2, 3, 4)
